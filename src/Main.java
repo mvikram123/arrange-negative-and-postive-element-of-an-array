a30c800ca64d7a4278336c0e2f0413c5fd3e77e8
manbodhvikram <manbodhvikram@gmail.com>
2023-07-05 12:05:46 +0530

    arrange negative and positive no.

---
+++ src/Main.java
@@ -0,0 +1,58 @@
+import java.util.*;
+
+class sortNegative{
+    public static void sortNegativeArray(int arr[],int n)
+    {
+        Arrays.sort(arr);
+        for(int i=0; i<n; i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
+}
+
+public class Main
+{
+    public static void main(String[] args) {
+        //	System.out.println("Hello World");
+        int arr[]={-1, 2, -3, 4, 5, 6, -7, 8, 9};
+        int n=arr.length;
+        sortNegative.sortNegativeArray(arr,n);
+    }
+}
+
+
+// another method
+//class GFG {
+//
+//    static void rearrange(int arr[], int n)
+//    {
+//        int j = 0, temp;
+//        for (int i = 0; i < n; i++) {
+//            if (arr[i] < 0) {
+//                if (i != j) {
+//                    temp = arr[i];
+//                    arr[i] = arr[j];
+//                    arr[j] = temp;
+//                }
+//                j++;
+//            }
+//        }
+//    }
+//
+//    // A utility function to print an array
+//    static void printArray(int arr[], int n)
+//    {
+//        for (int i = 0; i < n; i++)
+//            System.out.print(arr[i] + " ");
+//    }
+//
+//    // Driver code
+//    public static void main(String args[])
+//    {
+//        int arr[] = { -1, 2, -3, 4, 5, 6, -7, 8, 9 };
+//        int n = arr.length;
+//
+//        rearrange(arr, n);
+//        printArray(arr, n);
+//    }
+//}
